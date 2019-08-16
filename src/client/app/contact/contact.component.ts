9b9731c335631bda6ef5943312650f061c5d77e3
Doron Epstein <jimbeanzn@gmail.com>
2019-08-16 21:48:16 +0200

    feat: simplified and updated for 2019

--- src/client/app/contact/contact.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'dne-contact',
-  templateUrl: 'contact.component.html',
-  styleUrls: ['contact.component.css'],
-})
-export class ContactComponent { }
--- src/client/app/shared/header/header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'dne-header',
-  templateUrl: 'header.component.html',
-  styleUrls: ['header.component.css']
-})
-export class HeaderComponent { }
